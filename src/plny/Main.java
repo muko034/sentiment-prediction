c5d8cf463bccb5e6fe498082fd243ae612d0e72e
Zieciu <jakziec@gmail.com>
2015-05-12 15:25:42 +0200

    hashmapa

--- src/plny/Main.java
+++ src/plny/Main.java
@@ -3,13 +3,14 @@ package plny;
 import plny.model.*;
 
 import javax.xml.bind.JAXBException;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
         final String PATH = "sorted_data_acl/kitchen_&_housewares/negative.review";
-//        final String PATH = "sorted_data_acl/kitchen_&_housewares/positive.review";
+        final String PATH1 = "sorted_data_acl/kitchen_&_housewares/positive.review";
 //        final String PATH = "sorted_data_acl/kitchen_&_housewares/unlabeled.review";
 
         System.out.println("Hello World!");
@@ -17,11 +18,20 @@ public class Main {
         Input input = new Input();
 
         try {
-            input.read(PATH);
+            input.append(PATH);
+            input.append(PATH1);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
 
-        System.out.println(input.getReviews().get(5).getReview_text());
+        System.out.println(input.getReviews().size());
+        System.out.println(input.getReviews().get(1).size());
+        System.out.println(input.getReviews().get(1).get(5).getReview_text());
+        Set<Integer> keys = input.getReviews().keySet();
+        int sum = 0;
+        for (Integer i : keys) {
+            sum += input.getReviews().get(i).size();
+        }
+        System.out.println(sum);
     }
 }
--- src/plny/model/Input.java
+++ src/plny/model/Input.java
@@ -4,30 +4,43 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Zieciu on 2015-05-10.
  */
 public class Input {
 
-    public void read(String path) throws JAXBException {
+    public void append(String path) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Reviews.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
         Reviews revs = (Reviews) jaxbUnmarshaller.unmarshal( new File(path) );
 
-        this.reviews = revs.getReviews();
+        List<Review> list = null;
+        for (Review rev : revs.getReviews()) {
+            Integer rating = new Integer(new Double(rev.getRating()).intValue());
+            if (!this.reviews.containsKey(rating)) {
+                list = new ArrayList<Review>();
+                list.add(rev);
+                this.reviews.put(rating, list);
+            } else {
+                list = this.reviews.get(rating);
+                list.add(rev);
+            }
+        }
     }
 
-    public List<Review> getReviews() {
+    public Map< Integer, List<Review> > getReviews() {
         return reviews;
     }
 
-    public void setReviews(List<Review> reviews) {
+    public void setReviews(Map< Integer, List<Review> > reviews) {
         this.reviews = reviews;
     }
 
-    private List<Review> reviews = null;
-
+    private Map< Integer, List<Review> > reviews = new HashMap< Integer, List<Review> >(5, 1.0f);
 }
