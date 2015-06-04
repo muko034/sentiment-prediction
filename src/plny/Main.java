a97be89683d7ed93f2af2f5db3f279b5337d44cf
marcin <mrctrzcinski@gmail.com>
2015-06-04 18:21:47 +0200

    Ratingi i labele

--- src/plny/Main.java
+++ src/plny/Main.java
@@ -3,20 +3,22 @@ package plny;
 import plny.model.*;
 
 import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        final String PATH = "sorted_data_acl/kitchen_&_housewares/negative.review";
-        final String PATH1 = "sorted_data_acl/kitchen_&_housewares/positive.review";
+        final String PATH = "sorted_data_acl/books/negative.review";
+        final String PATH1 = "sorted_data_acl/books/positive.review";
 //        final String PATH = "sorted_data_acl/kitchen_&_housewares/unlabeled.review";
 
         System.out.println("Hello World!");
 
         Input input = new Input();
-        PolarityBasic pb = new  PolarityBasic();
+        //PolarityBasic pb = new  PolarityBasic();
 
         try {
             input.append(PATH);
@@ -24,31 +26,53 @@ public class Main {
         } catch (JAXBException e) {
             e.printStackTrace();
         }
-        Set<Integer> keys = input.getReviews().keySet();
+        PrintWriter zapis = new PrintWriter("rating/books_training.txt");
+        PrintWriter zapis2 = new PrintWriter("rating/books_evaluate.txt");
 
-        System.out.println("START TRAIN");
+        for(Integer i : input.getReviews().keySet())
+        {
 
-        for(Integer i : keys)
+            int amountToTrain= (int)(input.getReviews().get(i). size()* 0.75) ;
+            for(Review review : input.getReviews().get(i).subList(0, amountToTrain))
+            {
+                if(review == null)
+                    continue;
+                String result = review.getReview_text().replaceAll("[\\t\\n\\r]+", " ");
+                zapis.println(i.toString()+".0" + "\t"+result) ;
+            }
+            for(Review review : input.getReviews().get(i).subList(amountToTrain+1,input.getReviews().get(i).size()))
+            {
+                if(review == null)
+                    continue;
+                String result = review.getReview_text().replaceAll("[\\t\\n\\r]+", " ");
+                zapis2.println(i.toString()+".0" + "\t"+result) ;
+            }
+        }
+
+       /* input = new Input();
+        //PolarityBasic pb = new  PolarityBasic();
+
+        try {
+            input.append(PATH1);
+            //input.append(PATH1);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        for(Integer i : new Integer[]{4,5})
         {
             int amountToTrain= (int)(input.getReviews().get(i). size()* 0.75) ;
-            pb.train(input.getReviews().get(i).subList(0, amountToTrain));
-        }
-
-        System.out.println("START EVALUATE");
-        for(Integer i : keys)
-        {
-            int firstToEvaluate= (int)(input.getReviews().get(i). size()* 0.75)+1 ;
-            pb.evaluate(input.getReviews().get(i).subList(firstToEvaluate,input.getReviews().get(i).size()));
-        }
-
-        System.out.println(input.getReviews().size());
-        System.out.println(input.getReviews().get(1).size());
-        System.out.println(input.getReviews().get(1).get(5).getReview_text());
-
-        int sum = 0;
-        for (Integer i : keys) {
-            sum += input.getReviews().get(i).size();
-        }
-        System.out.println(sum);
+            for(Review review : input.getReviews().get(i).subList(0, amountToTrain))
+            {
+                String result = review.getReview_text().replaceAll("[\\t\\n\\r]+", " ");
+                zapis.println("positive\t"+result) ;
+            }
+            for(Review review : input.getReviews().get(i).subList(amountToTrain+1,input.getReviews().get(i).size()))
+            {
+                String result = review.getReview_text().replaceAll("[\\t\\n\\r]+", " ");
+                zapis2.println("positive\t"+result) ;
+            }
+        }*/
+        zapis.close();
+        zapis2.close();
     }
 }
