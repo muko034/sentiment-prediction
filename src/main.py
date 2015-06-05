94d832d639321850b5bc4bd8c9f6a3ceca8ca387
mtrzcinski <mtrzcinski@debian>
2015-06-05 15:12:38 +0200

    dodane do maina konwertowanie

--- src/main.py
+++ src/main.py
@@ -2,15 +2,25 @@
 
 from sys import argv, stderr
 from optparse import OptionParser
-
+import subprocess
+import os
 # CONSTANTS
-LIBSVM = "../libs/libsvm-3.20"
-LIBSHORTTEXT = "../libs/libshorttext-1.1"
+LIBSVM = "../libs/libsvm-3.20/"
+LIBSHORTTEXT = "../libs/libshorttext-1.1/"
+DANESVM="../svm_data/"
+DANELABELS="../labels/"
+DANERATING="../rating/"
+MODEL="../model/"
+RESULT="../result/"
 # [...]
 
 def main():
     print "hello world\n"
     options()
+    convert(DANELABELS+"books_training.txt","books_training.txt.svm")
+    convert(DANELABELS+"books_evaluate.txt","books_evaluate.txt.svm")
+    train(DANESVM+"books_evaluate.txt.svm")
+    predict(DANESVM+"books_evaluate.txt.svm","books_evaluate.txt.svm.model","books_result.txt")
 
 def options():
     usage = "usage: %prog [options] arg"
@@ -28,13 +38,33 @@ def options():
 
     (options, args) = parser.parse_args()
 
-    if len(args) != 1:
-        parser.error("incorrect number of arguments.\n\t\ttype \""+__file__+" -h\" for mor information")
+    #if len(args) != 1:
+        #parser.error("incorrect number of arguments.\n\t\ttype \""+__file__+" -h\" for mor information")
 
     if options.verbose:
         print "reading %s..." % options.filename
 
+def convert(src,dst):
+	print "python " + LIBSHORTTEXT + "text2svm.py "+ src + " " + DANESVM + dst
+	if not os.path.exists(DANESVM):
+    		os.makedirs(DANESVM)
+	if not os.path.exists(DANESVM+dst):
+		subprocess.call("python " + LIBSHORTTEXT + "text2svm.py "+ src + " " + DANESVM + dst, shell=True)
+
+def train(src):
+	tempdir=os.getcwd()
+	if not os.path.exists(MODEL):
+    		os.makedirs(MODEL)
+	os.chdir(MODEL)
+	print os.getcwd();
+	subprocess.call(LIBSVM + "svm-train -s 3 -p 0.1 -t 1 "+ src, shell=True)
+	os.chdir(tempdir)
+
+def predict(src,model,result):
+	if not os.path.exists(RESULT):
+    		os.makedirs(RESULT)
+	subprocess.call(LIBSVM + "svm-predict "+ src + " " + MODEL+model + " " + RESULT+result, shell=True)
+
 if __name__ == "__main__":
     main()
-
 
